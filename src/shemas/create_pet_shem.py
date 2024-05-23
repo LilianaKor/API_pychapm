5ea25f49d5bed05c5adf95755c426cb082d67f6a
Liliana <lilianakoric@gmail.com>
2024-05-22 22:40:24 -0700

    add Shema_pet in  test_create_pet.py

--- src/shemas/create_pet_shem.py
+++ src/shemas/create_pet_shem.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel, Field, field_validator
+from src.logger import get_logger
+
+logger = get_logger(__name__)
 
 class Category(BaseModel):
     id: int
     name: str
 
-class Tag(BaseModel) :
+
+class Tag(BaseModel):
     id: int
     name: str
+
 
 class CreatePetShema(BaseModel):
     id: int
@@ -15,3 +20,10 @@ class CreatePetShema(BaseModel):
     photoUrls: list[str] = []
     tag: list[Tag] = []
     status: str
+
+    @field_validator("status")
+    def check_status(cls, v: str) -> str:
+        if v not in ["available", "pending", "sold"]:
+            logger.error(f"Invalid status: {v}")
+            raise ValueError(f'Invalid status: {v}. Status must be one of ["available", "pending", "sold"]')
+

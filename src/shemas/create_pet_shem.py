0b2e62c26548b3ee8547c972f10108f040913bfe
Liliana <lilianakoric@gmail.com>
2024-05-22 22:28:22 -0700

    add Shema_pet in  test_create_pet.py

--- src/shemas/create_pet_shem.py
+++ src/shemas/create_pet_shem.py
@@ -1,3 +1,17 @@
 from pydantic import BaseModel, Field, field_validator
 
+class Category(BaseModel):
+    id: int
+    name: str
 
+class Tag(BaseModel) :
+    id: int
+    name: str
+
+class CreatePetShema(BaseModel):
+    id: int
+    name: str
+    category: Category
+    photoUrls: list[str] = []
+    tag: list[Tag] = []
+    status: str

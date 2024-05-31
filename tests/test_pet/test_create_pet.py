38e8517a6fbcf2d88a5c6092bc7017538415a76d
Liliana <lilianakoric@gmail.com>
2024-05-30 21:06:00 -0700

    Add test_create_pet.py, base_generator.py

--- tests/test_pet/test_create_pet.py
+++ tests/test_pet/test_create_pet.py
@@ -5,9 +5,11 @@ from src import CreatePetShem
 from src.assertions import Assertions
 from src.http_methods import MyRequests
 from http import HTTPStatus
-
+from src.prepare_data.prepare_pet_data import PreparePetData
+from src.shemas import CreatePetShemas
 from src.validator import Validator
 import json
+from generator.pet_generator import PetGenerator
 
 
 @allure.epic("Testing create pet")
@@ -16,44 +18,36 @@ class TestCreatePet:
     url = get_pet_endpoints()
     assertions = Assertions()
     validator = Validator()
+    pet_data = PreparePetData()
+    pet_generator = PetGenerator()
 
     def test_create_pet(self, get_test_name):
-        data = """{ 
-            "id": 35,
-            "category": {
-                "id": 0,
-                "name": "string777"
-            },
-            "name": "doggie71",
-            "photoUrls": [
-                "string"
-            ],
-            "tags": [
-                {
-                    "id": 888,
-                    "name": "string"
-                }
-            ],
-            "status": "available"              
-        }"""
-        response = self.request.post(url=self.url.create_pet, data=data)
-        print(response.text)
-        self.assertions.assert_status_code(
-            response=response,
-            actual_status_code=HTTPStatus.OK,
-            test_name=get_test_name
-         )
-        self.validator.validate_response(response=response, model=CreatePetShem.create_pet)
+        info = next(self.pet_generator.generate_pet())
+        print(info)
+        data = self.pet_data.prepare_pet_json(info)
+        print(data)
 
 
 
 
 
-        # # Validate the status code
+    # def test_create_pet(self, get_test_name):
+    #     info = next(self.pet_generator.generate_pet())
+    #     print(info)
+
+        # data = self.pet_data.get_pet_json()
+        # response = self.request.post(url=self.url.create_pet, data=data)
+        # print(response.text)
         # self.assertions.assert_status_code(
         #     response=response,
-        #     actual_status_code=HTTPStatus.CREATED,  # Ensure this is the expected status code
+        #     actual_status_code=HTTPStatus.OK,
         #     test_name=get_test_name
-        # )
+        #  )
+        # self.validator.validate_response(response=response, model=CreatePetShem.create_pet)
+
+
+
+
+
 
 
